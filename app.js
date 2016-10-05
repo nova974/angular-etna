c0d43deb0f115a508a6e74d640e12dd0fb7f4641
nova <gossard.william@gmail.com>
2016-10-05 10:46:23 +0200

    [+] add basic todo app

---
+++ app.js
@@ -0,0 +1,16 @@
+var app = angular.module("todoApp", []);
+
+app.controller("todoCtrl", function($scope) {
+    $scope.todos = [
+        { title: "Watch series", done: false },
+        { title: "Work", done: false },
+        { title: "Drink water or not", done: true}
+    ];
+
+    $scope.todoTitle = "";
+
+    $scope.addTodo = function() {
+        $scope.todos.push({ title: $scope.todoTitle, done: false });
+        $scope.todoTitle = "";
+    }
+})
